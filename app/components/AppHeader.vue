92e3c8069937eb896770b41b08ad587cc55e74dc
李水光 <liwtlt@cladonia.team>
2024-09-25 22:23:44 +0800

    feat: index page template and style

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,40 +1,19 @@
 <script setup lang="ts">
-import type { NavItem } from '@nuxt/content'
-
-const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
 </script>
 
 <template>
   <UHeader>
     <template #logo>
-      Kwai Video <UBadge
-        label="Downloader"
+      Kwai Video Downloader <UBadge
+        label="Online"
         variant="subtle"
+        color="green"
         class="mb-0.5 font-bold"
       />
     </template>
 
     <template #right>
-      <UButton
-        label="Sign in"
-        color="gray"
-        to="/login"
-      />
-      <UButton
-        label="Sign up"
-        icon="i-heroicons-arrow-right-20-solid"
-        trailing
-        color="black"
-        to="/signup"
-        class="hidden lg:flex"
-      />
-    </template>
-
-    <template #panel>
-      <UNavigationTree
-        :links="mapContentNavigation(navigation)"
-        default-open
-      />
+      <UColorModeToggle size="lg" />
     </template>
   </UHeader>
 </template>
