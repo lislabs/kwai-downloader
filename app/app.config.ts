21f5d371751c32fd508edc796f78c52ceb285d54
李水光 <liwtlt@cladonia.team>
2024-09-25 19:26:13 +0800

    chore: init with nuxt-ui-pro template

---
+++ app/app.config.ts
@@ -0,0 +1,42 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'blue',
+    gray: 'zinc',
+    variables: {
+      light: {
+        background: 'var(--color-gray-50)'
+      },
+      dark: {
+        background: 'var(--color-gray-950)'
+      }
+    },
+    button: {
+      rounded: 'rounded-full',
+      default: {
+        size: 'md'
+      }
+    },
+    input: {
+      default: {
+        size: 'md'
+      }
+    },
+    card: {
+      rounded: 'rounded-xl'
+    },
+    footer: {
+      top: {
+        wrapper: 'border-t border-gray-200 dark:border-gray-800',
+        container: 'py-8 lg:py-16'
+      },
+      bottom: {
+        wrapper: 'border-t border-gray-200 dark:border-gray-800'
+      }
+    },
+    page: {
+      hero: {
+        wrapper: 'lg:py-24'
+      }
+    }
+  }
+})
--- app/error.vue
+++ app/error.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 import type { ParsedContent } from '@nuxt/content'
+import type { NuxtError } from '#app'
+
+useSeoMeta({
+  title: 'Page not found',
+  description: 'We are sorry but this page could not be found.'
+})
+
+defineProps({
+  error: {
+    type: Object as PropType<NuxtError>,
+    required: true
+  }
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
 
 const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation(), { default: () => [] })
 const { data: files } = useLazyFetch<ParsedContent[]>('/api/search.json', { default: () => [], server: false })
@@ -12,7 +31,11 @@ provide('navigation', navigation)
     <AppHeader />
 
     <UMain>
-      <slot />
+      <UContainer>
+        <UPage>
+          <UPageError :error="error" />
+        </UPage>
+      </UContainer>
     </UMain>
 
     <AppFooter />
@@ -23,5 +46,7 @@ provide('navigation', navigation)
         :navigation="navigation"
       />
     </ClientOnly>
+
+    <UNotifications />
   </div>
 </template>
